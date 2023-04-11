466cadcddb584230e0aaffac98b0fda2c0e8ed74
uchennaofodile <56528037+uchennaofodile@users.noreply.github.com>
2023-04-11 12:24:42 -0400

    Change to app

--- src/App.js
+++ src/App.js
@@ -75,7 +75,10 @@ const App = () => {
                 ) : (
                     <div className="empty">
                         <h2>No movies found</h2>
+                        <hr/>
+                        <h6>Created By Uchenna Ofodile</h6>
                     </div>
+                    
                 )}
 
 
